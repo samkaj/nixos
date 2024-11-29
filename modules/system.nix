4fbf3d643f2692c1b3ba49291bd17758c8107382
samkaj <kajavasamuel@gmail.com>
2024-11-29 23:34:24 +0100

    [zsh] set default shell to zsh

--- modules/system.nix
+++ modules/system.nix
@@ -1,86 +1,102 @@
 {
-  pkgs,
-  lib,
-  ...
+	pkgs,
+		lib,
+		...
 }: {
-  users.users.samkaj = {
-    isNormalUser = true;
-    description = "samkaj";
-    extraGroups = ["networkmanager" "wheel"];
-  };
+	users.users.samkaj = {
+		isNormalUser = true;
+		description = "samkaj";
+		extraGroups = ["networkmanager" "wheel"];
+		shell = pkgs.zsh;
+	};
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+	nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  environment.pathsToLink = [ "/libexec" ];
+	environment.pathsToLink = [ "/libexec" ];
 
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    dates = lib.mkDefault "weekly";
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
+	nix.gc = {
+		automatic = lib.mkDefault true;
+		dates = lib.mkDefault "weekly";
+		options = lib.mkDefault "--delete-older-than 7d";
+	};
 
-  nixpkgs.config.allowUnfree = true;
+	nixpkgs.config.allowUnfree = true;
 
-  time.timeZone = "Europe/Stockholm";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sv_SE.UTF-8";
-    LC_IDENTIFICATION = "sv_SE.UTF-8";
-    LC_MEASUREMENT = "sv_SE.UTF-8";
-    LC_MONETARY = "sv_SE.UTF-8";
-    LC_NAME = "sv_SE.UTF-8";
-    LC_NUMERIC = "sv_SE.UTF-8";
-    LC_PAPER = "sv_SE.UTF-8";
-    LC_TELEPHONE = "sv_SE.UTF-8";
-    LC_TIME = "sv_SE.UTF-8";
-  };
+	time.timeZone = "Europe/Stockholm";
+	i18n.defaultLocale = "en_US.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "sv_SE.UTF-8";
+		LC_IDENTIFICATION = "sv_SE.UTF-8";
+		LC_MEASUREMENT = "sv_SE.UTF-8";
+		LC_MONETARY = "sv_SE.UTF-8";
+		LC_NAME = "sv_SE.UTF-8";
+		LC_NUMERIC = "sv_SE.UTF-8";
+		LC_PAPER = "sv_SE.UTF-8";
+		LC_TELEPHONE = "sv_SE.UTF-8";
+		LC_TIME = "sv_SE.UTF-8";
+	};
 
-  services.printing.enable = true;
+	services.printing.enable = true;
 
-  fonts = {
-    packages = with pkgs; [
-      material-design-icons
-      noto-fonts
-      noto-fonts-emoji
-      (nerdfonts.override {fonts = ["JetBrainsMono"];})
-    ];
+	fonts = {
+		packages = with pkgs; [
+			material-design-icons
+				noto-fonts
+				noto-fonts-emoji
+				(nerdfonts.override {fonts = ["JetBrainsMono"];})
+		];
 
-    enableDefaultPackages = false;
+		enableDefaultPackages = false;
 
-    fontconfig.defaultFonts = {
-      serif = ["Noto Sans" "Noto Color Emoji"];
-      sansSerif = ["Noto Sans" "Noto Color Emoji"];
-      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
-      emoji = ["Noto Color Emoji"];
-    };
-  };
+		fontconfig.defaultFonts = {
+			serif = ["Noto Sans" "Noto Color Emoji"];
+			sansSerif = ["Noto Sans" "Noto Color Emoji"];
+			monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+			emoji = ["Noto Color Emoji"];
+		};
+	};
 
-  environment.systemPackages = with pkgs; [
-    neovim
-    wget
-    curl
-    git
-    nnn
-    firefox
-    slack
-    alacritty
-    bash
-    psmisc
-  ];
+	environment.systemPackages = with pkgs; [
+		neovim
+			wget
+			curl
+			git
+			nnn
+			firefox
+			slack
+			alacritty
+			bash
+			psmisc
+			zsh
+	];
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  
-  services = {
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      pulse.enable = true;
-      alsa.support32Bit = true;
-    };
-  };
+	programs.zsh = {
+		enable = true;
+		shellAliases = {
+			ll = "ls -la";
+			gs = "git status";
+			ga = "git add";
+			gc = "git commit";
+			gp = "git push";
+			".." = "cd ..";
+			"..." = "cd ../..";
+			rebuild = "sudo nixos-rebuild switch";
+		};
+	};
 
-  networking.networkmanager.enable = true;
+	sound.enable = true;
+	hardware.pulseaudio.enable = false;
 
-  system.stateVersion = "24.05"; # Did you read the comment?
+	services = {
+		pipewire = {
+			enable = true;
+			alsa.enable = true;
+			pulse.enable = true;
+			alsa.support32Bit = true;
+		};
+	};
+
+	networking.networkmanager.enable = true;
+
+	system.stateVersion = "24.05"; # Did you read the comment?
 }
