f4a270d8af7346693bef6b3b0ea97a15881d13f9
samkaj <kajavasamuel@gmail.com>
2024-11-30 12:23:13 +0100

    [home-manager] initial setup

--- flake.nix
+++ flake.nix
@@ -3,14 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    home-manager = {
+	url = "github:nix-community/home-manager/release-24.05";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ { self, nixpkgs, ... }: {
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
 	    ./hosts/desktop/configuration.nix
+
+	    home-manager.nixosModules.home-manager  {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+		home-manager.users.samkaj = import ./home.nix;
+	    }
           ];
         };
       };
---
+++ home.nix
@@ -0,0 +1,28 @@
+{config, pkgs, ... }:
+{
+	home.username = "samkaj";
+	home.homeDirectory = "/home/samkaj";
+
+	home.packages = with pkgs; [
+		neofetch
+		zip
+		xz
+		unzip
+		ripgrep
+		jq
+		file
+		which 
+		tree
+	];
+
+	programs.git = {
+		enable = true;
+		userName = "samkaj";
+		userEmail = "kajavasamuel@gmail.com";
+	};
+
+
+
+	home.stateVersion = "24.05";
+	programs.home-manager.enable = true;
+}
