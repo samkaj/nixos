cf519a910cfba0b4a876b45d42d5daa194a10a3c
samkaj <kajavasamuel@gmail.com>
2024-12-02 20:32:36 +0100

    [nvim] add neovim module

---
+++ modules/neovim.nix
@@ -0,0 +1,10 @@
+{pkgs, lib, ...}:
+{
+  # Install formatter binaries
+  environment.systemPackages = with pkgs; [
+    rustfmt
+    python3Packages.black
+    clang-tools # For clang-format
+    nil # Nix language server
+  ];
+}
