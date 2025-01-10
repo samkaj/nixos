111912175cea17d63f95cc56f3ad092b594288fb
samkaj <kajavasamuel@gmail.com>
2025-01-10 23:24:36 +0100

    [feat] tweak lualine

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -141,7 +141,20 @@
         }
 
         require("telescope").setup{}
-        require("lualine").setup{}
+
+
+        require("lualine").setup{
+            options = {
+                theme = "auto",
+                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
+                icons_enabled = true,
+                globalstatus = true,
+            },
+            sections = {
+                lualine_a = { { 'mode', fmt = function(res) return res:sub(1,1) end } },
+            },
+        }
         require("harpoon").setup{}
 
         -- :Wq, Xa, etc. remaps
