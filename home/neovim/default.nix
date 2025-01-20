eff66f5fc8c5a47211a022ed8032957f302b960f
samkaj <kajavasamuel@gmail.com>
2025-01-20 21:14:02 +0100

    [feat] more base16

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  palette = config.colorScheme.palette;
+in
 {
   # Language servers for Rust, Python, JavaScript, HTML, CSS, TypeScript, C, C++
   programs.neovim = {
@@ -82,6 +85,12 @@
         
         vim.opt.background = "dark"
         vim.cmd("colorscheme base16-rose-pine")
+        require('base16-colorscheme').setup({
+           base00 = '#${palette.base00}', base01 = '#${palette.base01}', base02 = '#${palette.base02}', base03 = '#${palette.base03}',
+           base04 = '#${palette.base04}', base05 = '#${palette.base05}', base06 = '#${palette.base06}', base07 = '#${palette.base07}',
+           base08 = '#${palette.base08}', base09 = '#${palette.base09}', base0A = '#${palette.base0A}', base0B = '#${palette.base0B}',
+           base0C = '#${palette.base0C}', base0D = '#${palette.base0D}', base0E = '#${palette.base0E}', base0F = '#${palette.base0F}',
+        })
 
         require("tmux").setup()
 
